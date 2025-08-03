dfe85db5b3ff0075a2abd8d21e6d91b348b03303
npit-nhcho <nhcho@npit.co.kr>
2025-08-03 21:11:47 +0900

    Update tb_traffic_signal_fsm_solution.v

--- 2_RTL_design/solution/LAB2/tb_traffic_signal_fsm_solution.v
+++ 2_RTL_design/solution/LAB2/tb_traffic_signal_fsm_solution.v
@@ -5,38 +5,43 @@ module tb_traffic_signal_fsm();
   reg clk, reset;
   reg i_ta, i_tb;
   wire [1:0] o_sa, o_sb;
-  
+
   // internal parameter
   parameter clk_period = 10;
 
   // DUT
-  traffic_signal_fsm U0 (
+  trafficsignal_fsm U0 (
       .clk(clk),
       .reset(reset),
       .i_ta(i_ta), .i_tb(i_tb),    // sensor input
       .o_sa(o_sa), .o_sb(o_sb)     // signal light
   );
-  
+
   // Reset sequence
   initial begin
       reset = 0;
       #13 reset = 1;
       #(clk_period) reset = 0;
   end
-  
+
   // Clock generation
   always begin
       clk = 0;
       forever #(clk_period/2) clk = ~clk;
   end
-  
+
   // Stimulus
   initial begin
+      $dumpfile("waveform.vcd"); // dump waveform
+      $dumpvars(0, tb_traffic_signal_fsm); // dump variables
       i_ta = 0; i_tb = 0; #3;
       i_ta = 0; i_tb = 1; #(clk_period);
       i_ta = 1; i_tb = 0; #(clk_period);
-      i_ta = 0; i_tb = 1; #(clk_period);
-      i_ta = 0; i_tb = 0;
+      i_ta = 0;           #(2 * clk_period);
+      i_ta = 0; i_tb = 1; #(5 * clk_period);
+      i_ta = 0; i_tb = 0; #(5 * clk_period);
+      $finish;
   end
 
 endmodule
+~
